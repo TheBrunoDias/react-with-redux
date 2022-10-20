bbacb4fb6ccf12bb6582e31bd8acaff0fe930f24
TheBrunoDias <brunosousadias@hotmail.com>
2022-10-20 20:09:25 -0300

    initial mocks

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -2,7 +2,11 @@ import { Header } from '../../components/Header';
 import styles from './Home.module.scss';
 import Initial from '../../assets/inicial.png';
 
+import { categories } from '../../mocks/categories';
+import { useNavigate } from 'react-router-dom';
+
 export const Home: React.FC = () => {
+  const navigate = useNavigate();
   return (
     <>
       <Header
@@ -11,6 +15,20 @@ export const Home: React.FC = () => {
         image={Initial}
         className={styles.header}
       />
+
+      <div className={styles.categorias}>
+        <div className={styles['categorias-title']}>
+          <h1>Categorias</h1>
+        </div>
+        <div className={styles['categorias-container']}>
+          {categories.map((c, i) => (
+            <div key={i} onClick={() => navigate(`/categoria/${c.id}`)}>
+              <img src={c.thumbnail} alt={c.nome} />
+              <h1>{c.nome}</h1>
+            </div>
+          ))}
+        </div>
+      </div>
     </>
   );
 };
