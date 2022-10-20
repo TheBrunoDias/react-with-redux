aab4f2abee7b408267edf0240f4a6d6ab4cc090c
TheBrunoDias <brunosousadias@hotmail.com>
2022-10-20 19:13:29 -0300

    add react router dom, icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import { Navbar } from './components/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home } from './pages/Home';
 
 function App() {
   return (
     <>
-      <Navbar />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,11 +1,31 @@
 import styles from './Navbar.module.scss';
 import { ReactComponent as Logo } from '../../assets/logo.svg';
+import { Link, useLocation } from 'react-router-dom';
+import classNames from 'classnames';
+import { ShoppingCart } from 'phosphor-react';
 
 export const Navbar: React.FC = () => {
+  const { pathname } = useLocation();
   return (
     <nav className={styles.nav}>
       <Logo className={styles.logo} />
-      <p>Navbar</p>
+      <div>
+        <Link
+          to="/"
+          className={classNames(styles.link, {
+            [styles.selected]: pathname,
+          })}
+        >
+          Página Inicial
+        </Link>
+      </div>
+
+      <div className={styles.busca}></div>
+      <div className={styles.icones}>
+        <Link to="/carrinho">
+          <ShoppingCart weight={pathname === '/carrinho' ? 'fill' : 'regular'} size={24} />
+        </Link>
+      </div>
     </nav>
   );
 };
